812bb3462286f37d6dc7e06650a19dc2cbb1f505
Michał Ryć-Pacholczyk <s20453@pjwstk.edu.pl>
2025-03-04 14:55:07 +0100

    mod3

--- APBD-cw1/APBD-cw1/Program.cs
+++ APBD-cw1/APBD-cw1/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, burn world");
 Console.WriteLine("Hello, burn world more ");
+Console.WriteLine("Hello, burn world more x2 ");
+
 
